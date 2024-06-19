88dd34200c4a3781b8f339f06a234ce5fd7092aa
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-06-19 20:09:04 +0200

    Created Professor Post Type

--- functions.php
+++ functions.php
@@ -13,11 +13,16 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 
 function university_features() {
+    // For dynamic creation of menu
     // register_nav_menu('headerMenuLocation', 'Header Menu Location');
     // register_nav_menu('footerLocationOne', 'Footer Location One');
     // register_nav_menu('footerLocationTwo', 'Footer Location Two');
 
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+
 }
 
 add_action('after_setup_theme', 'university_features');
